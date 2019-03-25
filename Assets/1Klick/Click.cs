d6c0ea558a4c7f9659766351fcc9b22a0341e7c6
urbvik <urban.vikdahl@skelleftea.se>
2019-03-25 11:03:48 +0100

    skapade två demos en för klickning och en byt scen

---
+++ Assets/1Klick/Click.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Rotate(Vector3.up, 1f);
+    }
+
+    private void OnMouseDown()
+    {
+        print("Klickade på kuben");
+
+        Rigidbody rigid = GetComponent<Rigidbody>();
+        rigid.AddForce(transform.up * 300f);
+        rigid.AddForce(transform.right * 300f);
+        rigid.useGravity = true;
+    }
+}
---
+++ Assets/2BytScene/BytScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BytScene : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        print(sceneIndex);
+
+        if(sceneIndex==0)
+            SceneManager.LoadScene(1);
+        else
+            SceneManager.LoadScene(0);
+
+    }
+
+
+}
