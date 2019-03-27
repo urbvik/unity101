f3cf73aae3df7c66d8fd668260d990bd6b6adefa
urbvik <urban.vikdahl@skelleftea.se>
2019-03-27 10:36:11 +0100

    uppdaterade shoppen

--- Assets/3Varuhus/mouseHoverEffect.cs
+++ Assets/3Varuhus/mouseHoverEffect.cs
@@ -2,18 +2,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class mouseHoverEffect : MonoBehaviour
 {
+    public Transform GmeObjText;
+    public Transform GmeObjWallet;
+
     private void OnMouseEnter()
     {
-            gameObject.AddComponent<Outline>();
-            print("Outline Active");   
+        gameObject.AddComponent<Outline>();
+        GmeObjText.GetComponent<TextMeshPro>().text = name;
+        //print("Outline Active");   
     }
     private void OnMouseExit()
     {
         Destroy(GetComponent<Outline>());
-        print("Outline Removed");
+        GmeObjText.GetComponent<TextMeshPro>().text = "Shop";
+        //print("Outline Removed");
     }
-    
+
+    private void OnMouseDown()
+    {
+        int price = int.Parse(name.Split(' ')[1].Remove(3));
+        Destroy(gameObject);
+        GmeObjWallet.GetComponent<wallet>().Pay(price);
+        GmeObjText.GetComponent<TextMeshPro>().text = "Shop";
+    }
+
 }
---
+++ Assets/3Varuhus/wallet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class wallet : MonoBehaviour
+{
+    private int total = 1000;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<TextMeshPro>().text = "Wallet: " + total.ToString() + "$";
+    }
+
+    public void Pay(int price)
+    {
+        total -= price;
+    }
+}
