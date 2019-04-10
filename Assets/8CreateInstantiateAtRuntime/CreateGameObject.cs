066b72d81355c1c8b1cd131fc0cdf5e9c9b28d6a
urbvik <urban.vikdahl@skelleftea.se>
2019-04-10 12:21:01 +0100

    updaterad gameplan

---
+++ Assets/8CreateInstantiateAtRuntime/CreateGameObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGameObject : MonoBehaviour
+{
+    float offset = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.AddComponent<Rigidbody>();
+
+               
+                cube.transform.position = new Vector3(x + offset*x, y + offset*y, 0);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
